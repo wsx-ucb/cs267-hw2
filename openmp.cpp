88d051b548b0fce71adbd57f9ca8b331436f078c
Shuxian Wang <wsx@berkeley.edu>
2023-02-15 18:57:43 -0800

    Add bidirectional force application.

--- openmp.cpp
+++ openmp.cpp
@@ -1,9 +1,9 @@
 #include "common.h"
-#include <omp.h>
-#include <cmath>
-#include <vector>
 #include <algorithm>
+#include <cmath>
 #include <iostream>
+#include <omp.h>
+#include <vector>
 using std::vector;
 
 static int bn;
@@ -12,9 +12,11 @@ static vector<omp_lock_t> locks;
 
 void bin(particle_t* p, int i, int j, bool locking) {
     int idx = i * bn + j;
-    if (locking) omp_set_lock(&locks[idx]);
+    if (locking)
+        omp_set_lock(&locks[idx]);
     bins[idx].push_back(p);
-    if (locking) omp_unset_lock(&locks[idx]);
+    if (locking)
+        omp_unset_lock(&locks[idx]);
 }
 
 void init_simulation(particle_t* parts, int num_parts, double size) {
@@ -33,7 +35,7 @@ void init_simulation(particle_t* parts, int num_parts, double size) {
 }
 
 // Apply the force from neighbor to particle
-void apply_force(particle_t& particle, particle_t& neighbor) {
+void apply_force(particle_t& particle, particle_t& neighbor, bool bidirectional) {
     // Calculate Distance
     double dx = neighbor.x - particle.x;
     double dy = neighbor.y - particle.y;
@@ -50,6 +52,10 @@ void apply_force(particle_t& particle, particle_t& neighbor) {
     double coef = (1 - cutoff / r) / r2 / mass;
     particle.ax += coef * dx;
     particle.ay += coef * dy;
+    if (bidirectional) {
+        neighbor.ax -= coef * dx;
+        neighbor.ay -= coef * dy;
+    }
 }
 
 // Integrate the ODE
@@ -94,18 +100,51 @@ bool in_group(int i, int j, group const& g) {
 void simulate_one_step(particle_t* parts, int num_parts, double size) {
     #pragma omp barrier
     group g = get_group(omp_get_thread_num(), omp_get_num_threads());
+    // Clear particle acceleration initially
     for (int i = g.imin; i < g.imax; i++) {
         for (int j = g.jmin; j < g.jmax; j++) {
             for (particle_t* p : bins[i * bn + j]) {
                 p->ax = p->ay = 0;
-                for (int ni : {i-1, i, i+1}) {
+            }
+        }
+    }
+    // When possible, apply bidirectional force in the following pattern
+    // - - -
+    // - o *
+    // * * *
+    // for `o` representing the current bin,
+    // and `*` the bins we interact with,
+    // and `-` the bins we ignore.
+    for (int i = g.imin; i < g.imax; i++) {
+        for (int j = g.jmin; j < g.jmax; j++) {
+            vector<particle_t*>& bin = bins[i * bn + j];
+            for (particle_t* p : bin) {
+                for (int ni : {i - 1, i, i + 1}) {
                     if (ni < 0 || ni >= bn) continue;
-                    for (int nj : {j-1, j, j+1}) {
-                        if (nj < 0 || nj >= bn) continue;
-                        for (particle_t* n : bins[ni * bn + nj]) {
-                            apply_force(*p, *n);
+                    for (int nj : {j - 1, j, j + 1}) {
+                        // Skip out of bound bins and self,
+                        // since we handle self-interaction later
+                        if (nj < 0 || nj >= bn || (ni == i && nj == j)) continue;
+                        // If neighbor is in another group,
+                        // perform unidirectional force calculation only.
+                        if (!in_group(ni, nj, g)) {
+                            for (particle_t* n : bins[ni * bn + nj]) {
+                                apply_force(*p, *n, false);
+                            }
+                        // Now apply bidirectional forces with the `*` bins.
+                        } else if (nj < j || (nj == j && ni > i)) {
+                            for (particle_t* n : bins[ni * bn + nj]) {
+                                apply_force(*p, *n, true);
+                            }
                         }
                     }
+                }
+            }
+            // Handle interaction within the same bin,
+            // also using bidirectional force application
+            for (int p = 0; p < bin.size(); p++) {
+                for (int n = p + 1; n < bin.size(); n++) {
+                    apply_force(*bin[p], *bin[n], true);
                 }
             }
         }
@@ -120,18 +159,20 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
     for (int i = g.imin; i < g.imax; i++) {
         for (int j = g.jmin; j < g.jmax; j++) {
             vector<particle_t*>& b = bins[i * bn + j];
-            b.erase(std::remove_if(b.begin(), b.end(), [i, j, size, &in_group_moves, &out_group_moves, &g](particle_t* p) {
+            auto f = [i, j, size, &in_group_moves, &out_group_moves, &g](particle_t* p) {
                 move(*p, size);
                 int bi = p->x / cutoff;
                 int bj = p->y / cutoff;
-                if (bi == i && bj == j) return false;
+                if (bi == i && bj == j)
+                    return false;
                 if (in_group(bi, bj, g)) {
                     in_group_moves.push_back({p, bi, bj});
                 } else {
                     out_group_moves.push_back({p, bi, bj});
                 }
                 return true;
-            }), b.end());
+            };
+            b.erase(std::remove_if(b.begin(), b.end(), f), b.end());
         }
     }
     for (movement m : in_group_moves) {
